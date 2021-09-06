38defb78a5cf421c9cf0beb0664f2973a5e5a822
LAPTOP-04UR9CNS\user <jacky6632003@gmail.com>
2021-09-06 22:13:43 +0800

    加入專案檔案。

---
+++ Api/Controllers/SecretController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretController : Controller
+    {
+        [Authorize]
+        [HttpGet]
+        public string Index()
+        {
+            return "secret message";
+        }
+    }
+}
